05678f39691c6e8f9317e4501c2827a963756f95
judelle <sasha-lumpov@bk.ru>
2024-04-16 21:13:36 +0300

    third commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let completed = []
+
 const addTask = () => {
     const taskInput = document.getElementById("taskInput");
     const task = taskInput.value;
@@ -15,23 +17,71 @@ const addTask = () => {
   
     const deleteButton = document.createElement("button");
     deleteButton.innerHTML = "Delete";
-    deleteButton.classList.add("new"); 
+    deleteButton.classList.add("new");
     deleteButton.onclick = () => {
       taskList.removeChild(newTask);
       updateTaskCount();
     };
-    
-    
+
+    newTask.onclick = () => {
+      newTask.classList.add("completed");
+      completed.push(newTask)
+      updateTaskCount();
+
+    }
+     
     newTask.appendChild(taskText);
     newTask.appendChild(deleteButton);
     taskList.appendChild(newTask);
-  
+
     taskInput.value = "";
     updateTaskCount();
   };
 
+  function showCompleted() {
+    const taskList = document.querySelectorAll('.task')
+    console.log(taskList);
+    taskList.forEach(task => {
+      task.style.display = 'none'
+    })
+    completed.forEach((task) => {
+      task.style.display = "flex"
+    })
+  }
+  document.querySelector('button').addEventListener('click', () => {
+    
+  })
+
+  function showTasks() {
+    const taskList = document.querySelectorAll('.task')
+    console.log(taskList);
+    taskList.forEach(task => {
+      task.style.display = 'flex'
+    })
+    completed.forEach((task) => {
+      task.style.display = "none"
+    })
+  }
+  document.querySelector('button').addEventListener('click', () => {
+    
+  })
+
+  function showAll() {
+    const taskList = document.querySelectorAll('.task')
+    console.log(taskList);
+    taskList.forEach(task => {
+      task.style.display = 'flex'
+    })
+    completed.forEach((task) => {
+      task.style.display = "flex"
+    })
+  }
+  document.querySelector('button').addEventListener('click', () => {
+    
+  })
+
 const clearAll = () => {
-    const taskList = document.getElementById("taskList");
+  const taskList = document.getElementById("taskList");
     while (taskList.firstChild) {
         taskList.removeChild(taskList.firstChild);
     }
@@ -39,9 +89,11 @@ const clearAll = () => {
 };
 
 
+
+
 function updateTaskCount() {
+
   const taskCount = document.querySelectorAll('.task').length;
-  const taskCountElement = document.getElementById('taskCount');
-  taskCountElement.textContent = `${taskCount} tasks left`;
+  const taskCountElement = document.getElementById('taskCount');  taskCountElement.textContent = `${taskCount - completed.length} tasks left`;
 }
 
